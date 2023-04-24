1f31ed7262bf4e5a902ad5c3500b7a873e0e06b5
Benoit-Pierre STUDER <37024586+bpstuder@users.noreply.github.com>
2023-04-24 20:27:26 +0200

    Removed Gnome specific conf

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ echo "Setting up prerequisites"
 osName=$(hostnamectl | grep "Operating System" | awk -F": " {'print $2'})
 echo "Detected OS : $osName"
 
-echo "Installing Golang Role for Ansible"
-ansible-galaxy install fubarhouse.golang
+# echo "Installing Golang Role for Ansible"
+# ansible-galaxy install fubarhouse.golang
 
 echo "Installing Oh-My-ZSH Role for Ansible"
 ansible-galaxy install gantsign.oh-my-zsh
@@ -23,8 +23,8 @@ case $osName in
     *"Fedora"*)
         echo "Installing DNF-Automatic Role for Ansible"
         ansible-galaxy install exploide.dnf-automatic
-        echo "Install customize-gnome Role for Ansible"
-        ansible-galaxy install petermosmans.customize-gnome
+        # echo "Install customize-gnome Role for Ansible"
+        # ansible-galaxy install petermosmans.customize-gnome
     ;;
 
     "EndeavourOS"*|"Manjaro"*|"ArchLinux"*)
